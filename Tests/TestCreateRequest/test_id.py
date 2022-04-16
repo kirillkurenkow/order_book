5d4b99fecb07fbd8e9d7796b560cdc022979a5e4
Kirill Kurenkov <kkurenkov@parallels.com>
2022-04-16 11:48:12 +0300

    Add id tests

---
+++ Tests/TestCreateRequest/test_id.py
@@ -0,0 +1,24 @@
+import pytest
+from allure import (
+    step,
+    severity,
+    severity_level,
+)
+
+from OrderBook.Requests import Request
+from Tests.Source import (
+    attach_dict_to_report,
+    Defaults,
+)
+
+
+@severity(severity_level.BLOCKER)
+@pytest.mark.positive
+def test_id_order():
+    with step('Creating 5 requests'):
+        requests = [Request(price=Defaults.price, volume=Defaults.volume) for _ in range(5)]
+        base_id = requests[0].id
+        for i, request in enumerate(requests):
+            attach_dict_to_report(request.as_dict, name=f'Request {i + 1}')
+            assert request.id == base_id
+            base_id += 1
