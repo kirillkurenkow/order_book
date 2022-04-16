a1a3436c39f16ea4da1a72196bd43d623dfb33a5
Kirill Kurenkov <kkurenkov@parallels.com>
2022-04-16 12:53:00 +0300

    Add add_request tests

--- Tests/TestOrderBook/test_add_request.py
+++ Tests/TestOrderBook/test_add_request.py
@@ -1,7 +1,8 @@
 from allure import step, severity, severity_level
 import pytest
-from OrderBook.Requests import AskRequest, BidRequest
-from Source import attach_dict_to_report, Defaults, compare_request_with_request_info
+from OrderBook import RequestAlreadyExistsError, RequestError
+from OrderBook.Requests import AskRequest, BidRequest, Request
+from Tests.Source import attach_dict_to_report, Defaults, compare_request_with_request_info
 
 
 @pytest.mark.positive
@@ -14,8 +15,47 @@ def test_add_request(order_book, request_type):
     with step('Add request'):
         order_book.add_request(request)
     with step('Check request exists'):
-        snapshot = order_book.get_snapshot()
-        attach_dict_to_report(snapshot, 'Snapshot')
         request_info = order_book.get_request_info(request.id)
         attach_dict_to_report(request_info, 'Request info')
         compare_request_with_request_info(request, request_info)
+
+
+@pytest.mark.negative
+@severity(severity_level.CRITICAL)
+@pytest.mark.xfail(raises=RequestAlreadyExistsError, strict=True)
+def test_add_existing_request(order_book):
+    with step('Generate request'):
+        request = AskRequest(Defaults.price, Defaults.volume)
+        attach_dict_to_report(request.as_dict, 'Request')
+    with step('Add request'):
+        order_book.add_request(request)
+    with step('Add existing request'):
+        order_book.add_request(request)
+
+
+@pytest.mark.negative
+@severity(severity_level.CRITICAL)
+@pytest.mark.xfail(raises=RequestAlreadyExistsError, strict=True)
+def test_add_request_with_existing_id(order_book):
+    with step('Generate request'):
+        request = AskRequest(Defaults.price, Defaults.volume)
+        attach_dict_to_report(request.as_dict, 'Request')
+        request_with_the_same_id = BidRequest(Defaults.price, Defaults.volume)
+        request_with_the_same_id._id = request.id
+        attach_dict_to_report(request_with_the_same_id.as_dict, 'Request with the same id')
+    with step('Add request'):
+        order_book.add_request(request)
+    with step('Add request with the same id'):
+        order_book.add_request(request_with_the_same_id)
+
+
+@pytest.mark.negative
+@severity(severity_level.CRITICAL)
+@pytest.mark.parametrize('request_', [
+    AskRequest(Defaults.price, Defaults.volume).as_dict,
+    str(BidRequest(Defaults.price, Defaults.volume).as_dict),
+])
+@pytest.mark.xfail(raises=RequestError, strict=True)
+def test_add_not_a_request(order_book, request_):
+    with step('Add not a request'):
+        order_book.add_request(request_)
