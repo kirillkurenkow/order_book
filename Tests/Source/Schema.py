a928edceb86ffdd98e41753c0e56c65764fdcc77
Kirill Kurenkov <kkurenkov@parallels.com>
2022-04-17 13:12:45 +0300

    Add doc-strings

--- Tests/Source/Schema.py
+++ Tests/Source/Schema.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from schema import (
     Schema,
@@ -13,6 +14,9 @@ LOGGER = logging.getLogger(__name__)
 
 
 class OrderBookSchema:
+    """
+    Schemas for OrderBook
+    """
     request_info = Schema({
         'id': int,
         'price': Or(int, float),
@@ -26,10 +30,23 @@ class OrderBookSchema:
 
 
 class JsonSchemaValidationError(AssertionError):
+    """
+    Custom exception for validation error
+    Should inherit AssertionError to be product error (not broken test) in allure report
+    """
     ...
 
 
-def validate(data: dict, schema: Schema, attach_schema_to_report: bool = True) -> None:
+def validate(data: dict, schema: Schema, attach_schema_to_report: Optional[bool] = True) -> None:
+    """
+    Function for dict validation
+
+    :param data: Dict to validate
+    :param schema: Schema object
+    :param attach_schema_to_report: Attach json schema to allure report or not
+
+    :return: None
+    """
     LOGGER.info('Trying to validate dict with schema "%s"' % schema.name)
     if attach_schema_to_report:
         attach_dict_to_report(schema.json_schema(1), name='Validation schema')
--- Tests/Source/__init__.py
+++ Tests/Source/__init__.py
@@ -7,11 +7,27 @@ from Tests.OrderBook.Requests import Request
 
 
 def attach_dict_to_report(data: Union[list, dict], name: str) -> None:
+    """
+    Attach dict to allure report
+
+    :param data: Dict or list to attach
+    :param name: Attachment name
+
+    :return: None
+    """
     data = json.dumps(data, ensure_ascii=False, indent=4)
     allure.attach(data, name=name, attachment_type=allure.attachment_type.JSON)
 
 
 def compare_request_with_request_info(request: Request, request_info: dict) -> None:
+    """
+    Compare request object with request_info received from OrderBook.get_request_info method
+
+    :param request: Request object
+    :param request_info: request_info dict
+
+    :return: None
+    """
     assert request.price == request_info['price'], 'Wrong price in request_info'
     assert request.volume == request_info['volume'], 'Wrong volume in request_info'
     assert request.type == request_info['type'], 'Wrong type in request_info'
@@ -19,5 +35,9 @@ def compare_request_with_request_info(request: Request, request_info: dict) -> N
 
 
 class Defaults:
+    """
+    Class with default test values
+    """
+    # todo Add config?
     price = 100
     volume = 5
